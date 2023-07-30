49891d8a3ab5eb91cc0b2a72ea63d1ec86f371d5
Gabriel Bogo <gabrieldacruzbogo01@gmail.com>
2023-07-30 19:23:24 -0300

    modificando algumas verificacoes

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Cliente {
     private String nome;
-    private List <Endereco> enderecos = new ArrayList<>();
+    private List<Endereco> enderecos = new ArrayList<>();
 
-    private List <Pedido> pedidos = new ArrayList<>();
+    private List<Pedido> pedidos = new ArrayList<>();
 
-    public List <Cliente> clientes = new ArrayList<>();
+    public List<Cliente> clientes = new ArrayList<>();
+
+    int finalizadosTotais = 0;
+
+    int emAndamentoTotais = 0;
 
     Scanner scanner = new Scanner(System.in);
 
@@ -18,9 +22,10 @@ public class Cliente {
         this.enderecos = enderecos;
     }
 
-    public Cliente() {}
+    public Cliente() {
+    }
 
-    public void cadastrarCliente (){
+    public void cadastrarCliente() {
 
         System.out.println("\nPara fazer um pedido, insira os dados abaixo:\n");
 
@@ -36,59 +41,58 @@ public class Cliente {
         System.out.println("Digite o número da casa: ");
         int numCasa = scanner.nextInt();
 
-        Cliente novoCliente = new Cliente(nome,enderecos);
+        Cliente novoCliente = new Cliente(nome, enderecos);
         clientes.add(novoCliente);
 
 
-       Endereco novoEndereco = new Endereco(rua,bairro,numCasa);
-       enderecos.add(novoEndereco);
+        Endereco novoEndereco = new Endereco(rua, bairro, numCasa);
+        enderecos.add(novoEndereco);
 
         String nomePedido = "";
         int pedidoOpcao;
-do {
-        System.out.println("Digite o nº do seu pedido: ");
-        System.out.println("O cardápio de hoje é: ");
-        System.out.println("1 - Pizza de calabresa");
-        System.out.println("2 - X-Burguer");
-        System.out.println("3 - Batata-frita");
+        do {
+            System.out.println("Digite o nº da opção: ");
+            System.out.println("O cardápio de hoje é: ");
+            System.out.println("1 - Pizza de calabresa");
+            System.out.println("2 - X-Burguer");
+            System.out.println("3 - Batata-frita");
 
-        pedidoOpcao = scanner.nextInt();
+            pedidoOpcao = scanner.nextInt();
 
 
+            switch (pedidoOpcao) {
+                case 1:
+                    nomePedido = "Pizza de Calabresa";
+                    System.out.println("O seu pedido de Pizza de calabresa foi realizado!");
+                    break;
 
-    switch (pedidoOpcao) {
-        case 1:
-            nomePedido = "Pizza de Calabresa";
-            System.out.println("O seu pedido de Pizza de calabresa foi realizado!");
-            break;
+                case 2:
+                    nomePedido = "X-Burguer";
+                    System.out.println("O seu pedido de X-Burguer foi realizado!");
+                    break;
 
-        case 2:
-            nomePedido = "X-Burguer";
-            System.out.println("O seu pedido de X-Burguer foi realizado!");
-            break;
+                case 3:
+                    nomePedido = "Batata-frita";
+                    System.out.println("O seu pedido de Batata-Frita foi realizado!");
+                    break;
 
-        case 3:
-            nomePedido = "Batata-frita";
-            System.out.println("O seu pedido de Batata-Frita foi realizado!");
-            break;
-
-        default:
-            System.out.println("Digite uma opção válida");
-            break;
-    }
-}while (pedidoOpcao > 3);
+                default:
+                    System.out.println("Digite uma opção válida");
+                    break;
+            }
+        } while (pedidoOpcao > 3);
 
         System.out.println("Por último, digite o nº do pedido: ");
         int numPedido = scanner.nextInt();
 
         System.out.println("Pedido realizado com sucesso, muito obrigado!");
 
-        Pedido pedido = new Pedido(nomePedido,numPedido,false,false);
+        Pedido pedido = new Pedido(nomePedido, numPedido, false, false);
         pedidos.add(pedido);
     }
 
-    public void exibirDadosCliente (List<Cliente>clientes, List <Endereco> enderecos, List<Pedido> pedidos, Pedido pedido) {
-        for (int i = 0; i < clientes.size(); i++){
+    public void alterarStatusPedido(List<Cliente> clientes, List<Endereco> enderecos, List<Pedido> pedidos) {
+        for (int i = 0; i < clientes.size(); i++) {
             System.out.println("Nome: " + clientes.get(i).getNome());
             System.out.println("Rua: " + enderecos.get(i).getRua());
             System.out.println("Bairro: " + enderecos.get(i).getBairro());
@@ -97,15 +101,15 @@ do {
             System.out.println("Nº pedido: " + pedidos.get(i).getNumPedido());
             System.out.println("Descrição do pedido: " + pedidos.get(i).getNomePedido());
 
-            if (!pedidos.get(i).isAtivo()  && !pedidos.get(i).isFinalizado()){
+            if (!pedidos.get(i).isAtivo() && !pedidos.get(i).isFinalizado()) {
                 System.out.println("Status: Pedido realizado");
             }
 
-            if (pedidos.get(i).isAtivo()){
+            if (pedidos.get(i).isAtivo()) {
                 System.out.println("Status: Em andamento");
             }
 
-            if (pedidos.get(i).isFinalizado()){
+            if (pedidos.get(i).isFinalizado()) {
                 System.out.println("Status: Finalizado");
             }
 
@@ -113,10 +117,10 @@ do {
 
 
         }
-        finalizarPedido(pedidos, pedido);
+        finalizarPedido(pedidos);
     }
 
-    public void finalizarPedido (List<Pedido> pedidos, Pedido pedido){
+    public void finalizarPedido(List<Pedido> pedidos) {
         int opcao;
 
         if (pedidos.size() == 0) {
@@ -133,35 +137,46 @@ do {
                 switch (opcao) {
 
                     case 1:
+
+                        boolean verificaEmAndamento = false;
+
                         System.out.println("Digite o número do pedido que deseja colocar em produção:  ");
                         int numeroAtendimento = scanner.nextInt();
                         for (int i = 0; i < pedidos.size(); i++) {
                             if (numeroAtendimento == pedidos.get(i).getNumPedido()) {
                                 System.out.println("Pedido " + pedidos.get(i).getNumPedido() + " em produção!");
+                                emAndamentoTotais++;
                                 pedidos.get(i).setAtivo(true);
                                 pedidos.get(i).setFinalizado(false);
-                            } if (pedidos.get(i).getNumPedido() != numeroAtendimento){
-                                System.out.println("O número do pedido informado não existe");
+                                verificaEmAndamento = true;
                             }
+                        }
+                        if (!verificaEmAndamento) {
+                            System.out.println("O número do pedido não existe");
                         }
                         break;
 
                     case 2:
+                        boolean verificaFinalizado = false;
+
                         System.out.println("Digite o número do pedido que deseja finalizar");
                         int numeroFinalizar = scanner.nextInt();
                         for (int i = 0; i < pedidos.size(); i++) {
                             if (numeroFinalizar == pedidos.get(i).getNumPedido()) {
                                 System.out.println("Pedido " + pedidos.get(i).getNumPedido() + " finalizado!");
+                                finalizadosTotais++;
                                 pedidos.get(i).setAtivo(false);
                                 pedidos.get(i).setFinalizado(true);
-                            }
-                            if (pedidos.get(i).getNumPedido() != numeroFinalizar){
-                                System.out.println("O número do pedido informado não existe");
+                                verificaFinalizado = true;
                             }
                         }
+                        if (!verificaFinalizado) {
+                            System.out.println("O número do pedido não existe");
+                        }
+
                         break;
 
-                    case 3 :
+                    case 3:
                         break;
                     default:
                         System.out.println("Digite uma opção válida");
@@ -171,49 +186,76 @@ do {
         }
     }
 
-    public void quantidadePedidos (List <Pedido> pedidos){
+    public void quantidadePedidos(List<Pedido> pedidos, List<Cliente> clientes) {
+        for (int i = 0; i < clientes.size(); i++) {
+            System.out.println("Nome: " + clientes.get(i).getNome());
+            System.out.println("Rua: " + enderecos.get(i).getRua());
+            System.out.println("Bairro: " + enderecos.get(i).getBairro());
+            System.out.println("Nº casa: " + enderecos.get(i).getNumero());
+
+            System.out.println("Nº pedido: " + pedidos.get(i).getNumPedido());
+            System.out.println("Descrição do pedido: " + pedidos.get(i).getNomePedido());
+
+            if (!pedidos.get(i).isAtivo() && !pedidos.get(i).isFinalizado()) {
+                System.out.println("Status: Pedido realizado");
+            }
+
+            if (pedidos.get(i).isAtivo()) {
+                System.out.println("Status: Em andamento");
+            }
+
+            if (pedidos.get(i).isFinalizado()) {
+                System.out.println("Status: Finalizado");
+            }
+            System.out.println("");
+        }
         System.out.println("A quantidade total de pedidos é de: " + pedidos.size());
     }
 
-    public void pedidosFinalizados(List<Pedido> pedidos, List <Cliente> clientes) {
+    public void pedidosFinalizados(List<Pedido> pedidos, List<Cliente> clientes) {
         boolean verificaFinalizados = false;
 
         System.out.println("Pedidos finalizados: ");
 
-        for (int i =0; i< pedidos.size(); i++) {
+        for (int i = 0; i < pedidos.size(); i++) {
             if (pedidos.get(i).isFinalizado()) {
                 System.out.println("Pedido nº: " + pedidos.get(i).getNumPedido());
                 System.out.println("Descrição do pedido: " + pedidos.get(i).getNomePedido());
                 System.out.println("Nome do cliente: " + clientes.get(i).getNome());
+
                 verificaFinalizados = true;
 
                 System.out.println("");
             }
         }
+        System.out.println("\nPedidos finalizados totais: " + finalizadosTotais + "\n");
 
         if (!verificaFinalizados) {
             System.out.println("\nSem pedidos finalizados!\n");
         }
     }
 
-    public void pedidosEmProducao(List<Pedido> pedidos, List <Cliente> cliente){
+    public void pedidosEmProducao(List<Pedido> pedidos) {
 
         boolean verificaPedido = false;
 
         System.out.println("Pedidos em produção: ");
 
-        for (int i = 0; i< pedidos.size(); i++){
-            if (pedidos.get(i).isAtivo()){
+        for (int i = 0; i < pedidos.size(); i++) {
+            if (pedidos.get(i).isAtivo()) {
                 verificaPedido = true;
                 System.out.println("Número do pedido:" + pedidos.get(i).getNumPedido());
                 System.out.println("Nome do cliente:" + clientes.get(i).getNome());
                 System.out.println("Descrição do pedido: " + pedidos.get(i).getNomePedido());
 
                 System.out.println("");
+
+
             }
         }
+        System.out.println("A quantidade total de pedidos em andamento é de: " + emAndamentoTotais + "\n");
 
-        if (!verificaPedido){
+        if (!verificaPedido) {
             System.out.println("\nSem pedidos em andamento\n");
         }
     }
